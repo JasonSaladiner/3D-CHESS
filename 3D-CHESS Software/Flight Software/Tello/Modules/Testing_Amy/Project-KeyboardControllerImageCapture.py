592be98567861c0dc4136ef6d38f762e87c54fec
amysarah15 <amysarah15@tamu.edu>
2023-02-24 12:26:01 -0600

    Uploading Personal Files
    Tada

---
+++ 3D-CHESS Software/Flight Software/Tello/Modules/Testing_Amy/Project-KeyboardControllerImageCapture.py
@@ -0,0 +1,42 @@
+from djitellopy import Tello
+import KeyPressModule as kp
+from time import sleep, time
+import cv2
+
+kp.init()
+me = Tello()
+me.connect()
+print(me.get_battery())
+global img
+me.streamon()
+sleep(10)
+
+def getKeyboardInput():
+    lr, fb, ud, yv = 0, 0, 0, 0
+    spd = 50
+
+    if kp.getKey("LEFT"): lr = -spd
+    elif kp.getKey("RIGHT"): lr = spd
+    if kp.getKey("UP"): fb = spd
+    elif kp.getKey("DOWN"): fb = -spd
+    if kp.getKey("w"): ud = spd
+    elif kp.getKey("s"): ud = -spd
+    if kp.getKey("a"): yv = -spd
+    elif kp.getKey("d"): yv = spd
+    if kp.getKey("e"): me.takeoff()
+    if kp.getKey("q"): me.land()
+
+    if kp.getKey("p"):
+        cv2.imwrite(f'Resources/Images/{time.time()}.jpg', img)
+        sleep(2)
+
+    return [lr, fb, ud, yv]
+
+me.set_video_direction(1)
+while True:
+    vals = getKeyboardInput()
+    me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    img = me.get_frame_read().frame
+    img = cv2.resize(img, (360, 240))
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
