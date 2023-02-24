30a5852d180be522f71e275291f14ab24e455488
amysarah15 <amysarah15@tamu.edu>
2023-02-24 17:17:08 -0600

    Goes to XY Coords and lands

--- 3D-CHESS Software/Flight Software/Tello/Modules/Location/Task__GoXY_Pic_RecLXY_RetOG.py
+++ 3D-CHESS Software/Flight Software/Tello/Modules/Location/Task__GoXY_Pic_RecLXY_RetOG.py
@@ -1,4 +1,6 @@
 from djitellopy import Tello
+import cv2
+import time
 
 # User Input for Coordinates
 print('Input XY coordinates in centimeters: ')
@@ -10,7 +12,11 @@ tello = Tello()
 tello.connect()
 print("Battery: " + str(tello.get_battery()))
 
+# Go to XY Coordinates
 tello.takeoff()
-tello.go_xyz_speed(y_ui, -x_ui, 0, 30)  # ADJUST SPEED AS DESIRED
+tello.go_xyz_speed(y_ui, -x_ui, 20, 30)  # ADJUST SPEED AS DESIRED
+time.sleep(2)
+
+# Capture image at specified location
 tello.land()
 quit()
