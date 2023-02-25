9b2712818828c6018c0db4a97300d50cf5bb7474
amysarah15 <amysarah15@tamu.edu>
2023-02-24 18:29:22 -0600

    fixed dumb error

--- 3D-CHESS Software/Flight Software/Tello/Modules/Location/Task__GoXY_Pic_RecLXY_RetOG.py
+++ 3D-CHESS Software/Flight Software/Tello/Modules/Location/Task__GoXY_Pic_RecLXY_RetOG.py
@@ -18,6 +18,7 @@ def TakePhoto(tello):
     img = tello.get_frame_read().frame
     cv2.resize(img, (320, 240))
     cv2.imwrite(f'Images/{time.time()}.jpg', img)
+    tello.streamoff()
 
 
 # User Input for Coordinates
