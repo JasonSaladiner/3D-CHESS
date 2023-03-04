ff7bf14ff31c5867bb73a797bec3b3c2678e61d7
JasonSaladiner <saladiner.jason@tamu.edu>
2023-03-03 21:34:54 -0600

    fixed/improved TFS
    TelloFlightSoftware is the new subclass used to do our calling across branches

--- 3D-CHESS Software/Flight Software/Tello/FlightMain.py
+++ 3D-CHESS Software/Flight Software/Tello/FlightMain.py
@@ -7,7 +7,8 @@ From here, all other flight modules will be controlled in order to achieve 3D-CH
 
 import ipaddress
 import logging  #For changing the tello outputs
-from djitellopy import Tello as djiTello   #Tello 
+#from djitellopy import Tello as djiTello   #Tello 
+from Modules import TelloFlightSoftware as TFS
 import threading    #For multithreading
 import cv2
 
@@ -19,81 +20,17 @@ from Modules.Location import IMU,Mapping            #Location services
 
 from time import sleep
 
-haveLogMessages = False
-if not haveLogMessages:
-    djiTello.LOGGER.setLevel(logging.WARNING)      #Setting tello outpus to warning only
-
-
-class Tello(djiTello):
-    
-    """
-    Subclass that inherits all of the djitellopy Tello class (here called djiTello)
-    also has internet and thread intializations specific to 3dChess
-    """
-
-    #Location Thread
-    allowLocation = True
-    #Mapping Thread
-    allowMap = False
-    showMap = True
-
-
-    def threadSetup(self):
-        """
-        Initalizes selected threads
-        allowLocation = True : initializes the IMU location service thread to update cfg.X,Y,Z
-        allowMap = True : starts the live mapping service
-            showMap = True : outputs the map. 
-        """
-
-        if self.haveLocation:
-            self.locationThread = threading.Thread(target=IMU.init,args=(self.t,),)
-            self.locationThread.start()
-
-        #Initialize the map and determine if it will be seen
-                    ###THIS IS UNTESTED###
-        if self.haveMap and self.haveLocation:        #map depends on Location
-            self.mapThread = threading.Thread(target=Mapping.init,args=(self.showMap,),)
-            self.mapThread.start()
-
-
-    def wifi(self,SSID:str = 'tellonet',password:str = 'selvachess'):
-        """
-        connect the drone to the specific wifi (default is tellonet)
-        """
-        
-        self.t.connect_to_wifi(SSID,password)
-    
-
-    def runMission(self,mission:function,*args,**kwargs):
-        """
-        Runs a specific mission function
-        Very basic first set up. Might delete or update 
-        """
-
-        self.re = mission(*args,**kwargs)
-        if self.re != None:
-            return self.re
-
-
-    def __init__(self,IP:str):
-        """
-        initialize the drone and connect to it
-        """
-        self.t = djiTello(IP)
-        self.t.connect()
-        print(self.t.get_battery())
 
 
 #Entrance
 if __name__ == "__main__":
-    tello_B = Tello(cfg.telloIP_B)
+    tello_B = TFS(cfg.telloIP_B)
     #tello_B.wifi()
     tello_B.threadSetup()
-
-    tello_B.runMission(cc.move_to_waypoints,tello_B,cc.sampleWaypoints)
+    #tello_B.runMission(mc.EngageMC,tello_B)
+    #tello_B.runMission(cc.move_to_waypoints,tello_B,cc.sampleWaypoints)
 
 
     #Start manual control
-    #mc.EngageMC(tello_B)
+    mc.EngageMC(tello_B)
     
--- 3D-CHESS Software/Flight Software/Tello/Modules/__init__.py
+++ 3D-CHESS Software/Flight Software/Tello/Modules/__init__.py
@@ -1,3 +1,91 @@
 ##Modules __init__
 
 __all__ = []
+
+
+from djitellopy import Tello as djiTello
+import logging
+import threading
+
+
+
+class TelloFlightSoftware(djiTello):
+    
+    """
+    Subclass that inherits all of the djitellopy Tello class (here called djiTello)
+    also has internet and thread intializations specific to 3dChess
+    """
+
+
+
+    def threadSetup(self):
+        """
+        Initalizes selected threads
+        allowLocation = True : initializes the IMU location service thread to update cfg.X,Y,Z
+        allowMap = True : starts the live mapping service
+            showMap = True : outputs the map. 
+        """
+
+        if self.haveLocation:
+            from Modules.Location import IMU
+            self.locationThread = threading.Thread(target=IMU.init,args=(self.t,),)
+            self.locationThread.start()
+
+        #Initialize the map and determine if it will be seen
+                    ###THIS IS UNTESTED###
+        if self.haveMap and self.haveLocation:        #map depends on Location
+            from Modules.Location import Mapping
+            self.mapThread = threading.Thread(target=Mapping.init,args=(self.showMap,),)
+            self.mapThread.start()
+
+
+    def wifi(self,SSID:str = 'tellonet',password:str = 'selvachess'):
+        """
+        connect the drone to the specific wifi (default is tellonet)
+        """
+        
+        self.t.connect_to_wifi(SSID,password)
+    
+
+    def runMission(self,mission,*args,**kwargs):
+        """
+        Runs a specific mission function
+        Very basic first set up. Might delete or update 
+        """
+
+        self.re = mission(*args,**kwargs)
+        if self.re != None:
+            return self.re
+
+
+    def __init__(self,IP:str,**kwargs):
+        """
+        initialize the drone and connect to it
+        """
+        self.haveLogs = True
+        #Location Thread
+        self.haveLocation = True
+        #Mapping Thread
+        self.haveMap = False
+        self.showMap = True
+
+        for self.k in kwargs:
+            if self.k == 'logs':
+                self.haveLogs = kwargs[self.k]
+            elif self.k == 'location':
+                self.haveLocation = kwargs[self.k]
+            elif self.k == 'map':
+                self.haveMap = kwargs[self.k]
+            elif self.k == 'showmap':
+                self.showMap = kwargs[self.k]
+        if not self.haveLogs:
+            djiTello.LOGGER.setLevel(logging.WARNING)      #Setting tello outpus to warning only
+
+
+
+        self.t = super()
+        self.t.__init__(IP)
+        self.t.connect()
+
+        print(self.t.get_battery())
+
