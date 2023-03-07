1f5f029909ca996d5859cd71bbb2a5feefe502f8
amysarah15 <amysarah15@tamu.edu>
2023-03-07 15:31:10 -0600

    error check
    raises error values

--- 3D-CHESS Software/Flight Software/Tello/Modules/ImageProcessing/LiveVideo.py
+++ 3D-CHESS Software/Flight Software/Tello/Modules/ImageProcessing/LiveVideo.py
@@ -9,19 +9,29 @@ global img
 
 
 def startVideo(ConnectedTello, streamType='Live'):
+    # Error if streamType not valid
+    streamTypes = ['Live', 'FT']
+    if streamType not in streamTypes:
+        raise ValueError("Invalid streamType type. Expected one of: %s" % streamTypes)
+    # Create naming scheme for cv2 windows
+    t = time.localtime()
+    t_name = time.strftime("%H%M%S", t)
+    # Initialize function + video connection
     tello = ConnectedTello
-    tello.query_battery()  # testing purposes
+    tello.query_battery()  # testing purposes | DEMO
     tello.streamon()
-    time.sleep(2)
+    time.sleep(2) # adjust as needed
     tello.set_video_direction(0)
-    time.sleep(2)
+    time.sleep(2) # adjust as needed
 
     while streamType == 'Live':
         img = tello.get_frame_read().frame
         img = cv2.resize(img, (360, 240))
-        cv2.imshow("Image", img)
+        cv2.imshow("LStream"+t_name, img)  # untested, should name view w/ timestamp
         cv2.waitKey(5)
 
+    while streamType == 'FT':
+        pass
 
 
 
