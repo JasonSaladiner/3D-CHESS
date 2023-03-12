02e995588a697b55214b044c45214e434878f86d
amysarah15 <amysarah15@tamu.edu>
2023-03-12 12:49:16 -0700

    Re-setting up

--- 3D-CHESS Software/Flight Software/Tello/FlightMain.py
+++ 3D-CHESS Software/Flight Software/Tello/FlightMain.py
@@ -24,22 +24,5 @@ from time import sleep
 
 #Entrance
 if __name__ == "__main__":
-    #tello_C = TFS(cfg.telloIP_C,emControl = True)
-    #tello_C.threadSetup()
-    # OFF IN SAN FRAN, ADJUSTING FOR NO ROUTER ACCESS
-    tello_C = TFS('192.168.10.1', emControl = True)
-
-<<<<<<< Updated upstream
-=======
-    tello_C = Tello(cfg.telloIP_C)
-    tello_C.connect()
-    # DIDN'T MERGE YET, SO WILL DISAPPEAR AND HAVE THREADING FUNCTION IN TFS?
-    mc = threading.Thread(target=mc.EngageMC, daemon=False, args=(tello_C, ))
-    ft =threading.Thread(target=lv.startVideo, daemon=False, args=(tello_C, 'FT', True))
-    ft.start()
-    sleep(10)
-    mc.start()
->>>>>>> Stashed changes
-
-
-   
+    tello_C = TFS(cfg.telloIP_C,emControl = True)
+    tello_C.threadSetup()
