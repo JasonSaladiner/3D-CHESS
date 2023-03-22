456b1912fbffa0ec1b4d538b90f849900fcf91aa
saladiner-jason <saladiner.jason@tamu.edu>
2023-03-22 11:33:11 -0500

    Created Constellation Design
    Outside of TFS, Constellation Design is used for the Capstone Full Mission PDR. Will select walker constellation numbers and altitudes and generate figures

---
+++ 3D-CHESS Software/Flight Software/Orbit Selection/Constellation Design.py
@@ -0,0 +1,11 @@
+"""
+Code will implement STK API to select an optimal walker constellation and generate visuals
+"""
+
+#Imports
+from shutil import ReadError
+from agi.stk12.stkengine import STKEngine
+from agi.stk12.stkdesktop import STKDesktop
+from agi.stk12.stkobjects import *
+from agi.stk12.stkutil import *
+
