23550c9e5238dc7e31273c50841f88cf67a4fb9c
amysarah15 <amysarah15@tamu.edu>
2023-03-23 12:03:14 -0500

    some stuff

--- 3D-CHESS Software/Flight Software/Tello/Modules/ImageProcessing/LiveVideo.py
+++ 3D-CHESS Software/Flight Software/Tello/Modules/ImageProcessing/LiveVideo.py
@@ -22,7 +22,7 @@ def findFace(img):
     dir = current_dir + '/Flight Software/Tello/Resources/facedetect.xml'
     faceCascade = cv2.CascadeClassifier(dir)
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(imgGray, 1.2, 15)  # adjust minNeighbors as needed for clarity, OG = 8
+    faces = faceCascade.detectMultiScale(imgGray, 1.2, 10)  # adjust minNeighbors as needed for clarity, OG = 8
 
     myFaceListC = []  # cx, cy = center of face detected
     myFaceListArea = []
@@ -44,7 +44,7 @@ def findFace(img):
 
 
 # Royal Functions
-def startVideo(ConnectedTello, TelloName, streamType='FT', takePic=False):
+def startVideo(ConnectedTello, TelloName, streamType='FT', streamShow = False, takePic=False):
     # Error if NameTello, streamType, or takePic not valid
     TelloNames = ['tello_A', 'tello_B', 'tello_C']
     streamTypes = ['Live', 'FT']
@@ -79,25 +79,27 @@ def startVideo(ConnectedTello, TelloName, streamType='FT', takePic=False):
     time.sleep(2) # adjust as needed
     global start_time
     alert_status = False
+    tello.send_expansion_command("led 255 0 0")
 
     while streamType == 'Live':
         img = tello.get_frame_read().frame
         img = cv2.resize(img, (w, h))
         cv2.putText(img, TelloName, (20, 30), cv2.FONT_HERSHEY_PLAIN, 2, TelloColor, 2)
-        cv2.imshow("LStream" + t_name, img)
+        if streamShow == True:
+            cv2.imshow("LStream" + t_name, img)
         cv2.waitKey(5)
 
     while streamType == 'FT':
+        time.sleep(2)
         img_base = tello.get_frame_read().frame
-        img, info = findFace(img_base)
+        imgFT, info = findFace(img_base)
         area_val = info[1]
-        alert = 'OBJECT DETECTED BY'
+        alert = ': OBJECT DETECTED'
         if area_val != 0 and start_time == 0:
             alert_status = True
             start_time = time.time()
-            print(alert, TelloName)
             if takePic == True:
-                cv2.imwrite(f'Flight Software/Tello/Resources/Images/{time.time()}.jpg', img_base)
+                cv2.imwrite(f'Flight Software/Tello/Resources/Images/{time.time()}.jpg', imgFT)
         elif area_val == 0 and time.time() - start_time > buffer:
             alert_status = False
             start_time = 0
@@ -105,7 +107,10 @@ def startVideo(ConnectedTello, TelloName, streamType='FT', takePic=False):
         cv2.putText(img, TelloName, (20, 30), cv2.FONT_HERSHEY_PLAIN, 2, TelloColor, 2)
         if alert_status == True:
             cv2.putText(img, alert, (300, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 2)
-        #cv2.imshow("FTStream" + t_name, img)
+        if streamShow == True:
+            cv2.imshow("FTStream" + t_name, imgFT)
+        else:
+            print(TelloName, alert)
         cv2.waitKey(5)
 
 
