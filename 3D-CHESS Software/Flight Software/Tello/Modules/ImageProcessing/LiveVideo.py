9d224049e576c9da17256f11f3d12ab3cac9944d
JasonSaladiner <saladiner.jason@tamu.edu>
2023-04-17 20:44:44 -0500

    FT "RollBack" Attempt

--- 3D-CHESS Software/Flight Software/Tello/Modules/ImageProcessing/LiveVideo.py
+++ 3D-CHESS Software/Flight Software/Tello/Modules/ImageProcessing/LiveVideo.py
@@ -5,17 +5,44 @@ from djitellopy import Tello
 import time
 import cv2
 import os
-import Modules._config_ as cfg  
 
 
 # Global variables + parameters
-global img, img_base, buffer
+global img, img_base
+global buffer
 start_time = 0
 buffer = 3  # Adjust according to speed of Tello
-currentDir = os.getcwd()
 
 
-# Royal Function
+# Subject Function
+def findFace(img):
+    # Pull current directory for any user
+    current_dir = os.getcwd()
+    dir = current_dir + '/Flight Software/Tello/Resources/facedetect.xml'
+    faceCascade = cv2.CascadeClassifier(dir)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray, 1.2, 10)  # adjust minNeighbors as needed for clarity, OG = 8
+
+    myFaceListC = []  # cx, cy = center of face detected
+    myFaceListArea = []
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cx = x + (w // 2)
+        cy = y + (h // 2)
+        area = w * h
+        cv2.circle(img, (cx, cy), 5, (0, 255, 0), cv2.FILLED)
+        myFaceListC.append([cx, cy])
+        myFaceListArea.append(area)
+
+    if len(myFaceListArea) != 0:
+        i = myFaceListArea.index(max(myFaceListArea))
+        return img, [myFaceListC[i], myFaceListArea[i]]
+    else:
+        return img, [[0, 0], 0]
+
+
+# Royal Functions
 def startVideo(ConnectedTello, streamType='FT', streamShow = True, takePic=False):
     # Error if streamType or takePic not valid
     streamTypes = ['Live', 'FT']
@@ -25,68 +52,80 @@ def startVideo(ConnectedTello, streamType='FT', streamShow = True, takePic=False
     if takePic not in takePics:
         raise ValueError("Invalid takePic type. Expected one of: %s" % takePics)
 
+    ## Create cv2 window for TelloVision
+    #cv2.namedWindow("TelloVision", cv2.WINDOW_NORMAL)
+    #cv2.moveWindow("TelloVision", 0, 0)
+    #cv2.resizeWindow("TelloVision", 640, 1440)
+
+    # Color identities for each Tello
+    TelloColors = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]
+    TelloColor = None
+    if ConnectedTello.name == "Tello_A":
+        TelloColor = TelloColors[0]
+    elif ConnectedTello.name == "Tello_B":
+        TelloColor = TelloColors[1]
+    elif ConnectedTello.name == "Tello_C":
+        TelloColor = TelloColors[2]
+
+    # Create naming scheme for cv2 windows
+    t = time.localtime()
+    t_name = time.strftime("%H%M%S", t)
+
     # Initialize function + video connection
     tello = ConnectedTello
     tello.query_battery()  # testing purposes | DEMO
     tello.streamon()
     time.sleep(2) # adjust as needed
-    global start_time, ind1, ind2
+    #tello.set_video_direction(0) # Errors out randomly
+    time.sleep(2) # adjust as needed
+    global start_time
     alert_status = False
 
     while streamType == 'Live':
         imgL = tello.get_frame_read().frame
         imgL = cv2.resize(imgL, (400, 300))
+        
         if streamShow == True:
             if ConnectedTello.name == "Tello_A":
-                cv2.imshow(ConnectedTello.name, imgL)
-                cv2.moveWindow(ConnectedTello.name, 0, 0)
+                cv2.imshow("ConnectedTello.name", imgL)
+                cv2.moveWindow("ConnectedTello.name", 0, 0)
             if ConnectedTello.name == "Tello_B":
-                cv2.imshow(ConnectedTello.name, imgL)
-                cv2.moveWindow(ConnectedTello.name, 0, 300)
+                cv2.imshow("ConnectedTello.name", imgL)
+                cv2.moveWindow("ConnectedTello.name", 0, 300)
             if ConnectedTello.name == "Tello_C":
-                cv2.imshow(ConnectedTello.name, imgL)
-                cv2.moveWindow(ConnectedTello.name, 0, 600)
+                cv2.imshow("ConnectedTello.name", imgL)
+                cv2.moveWindow("ConnectedTello.name", 0, 600)
         cv2.waitKey(5)
 
     while streamType == 'FT':
+        #time.sleep(2) # if errors w/ first frame grab
         imgFT = tello.get_frame_read().frame
-        predictions, index = myClassifier.getPrediction(imgFT, scale=1, pos=(0, 30))
-        if start_time == 0 and index != 0:
+        imgFT, info = findFace(imgFT)
+        area_val = info[1]
+        alert = 'OBJECT DETECTED'
+        if area_val != 0 and start_time == 0:
+            alert_status = True
             start_time = time.time()
-        if time.time() - start_time < buffer:
-            if index == 1:
-                ind1 += 1
-            elif index ==2:
-                ind2 += 1
-        elif time.time() - start_time > buffer:
+            if takePic == True:
+                cv2.imwrite(f'Flight Software/Tello/Resources/Images/{time.time()}.jpg', imgFT)
+        elif area_val == 0 and time.time() - start_time > buffer:
+            alert_status = False
             start_time = 0
-            if ind1 > ind2 and ind1 >= indmin:
-                print('HIGH DETECTION: CUP')
-                location = ConnectedTello.position
-                x = location[0][0]
-                y = location[1][0]
-                cfg.task_requests.append(cfg.Task([x, y]))
-            elif ind2 > ind1 and ind2 >= indmin:
-                print('HIGH DETECTION: SLEEVE')
-                x = location[0][0]
-                y = location[1][0]
-                cfg.task_requests.append(cfg.Task([x, y]))
-            ind1 = 0
-            ind2 = 0
         imgFT = cv2.resize(imgFT, (400, 300))
+        if alert_status == True:
+            cv2.putText(imgFT, alert, (250, 30), cv2.FONT_HERSHEY_PLAIN, .85, (0, 255, 255), 2)
         if streamShow == True:
             if ConnectedTello.name == "Tello_A":
-                cv2.imshow(ConnectedTello.name, imgFT)
-                cv2.moveWindow(ConnectedTello.name, 0, 0)
+                cv2.imshow("ConnectedTello.name", imgFT)
+                cv2.moveWindow("ConnectedTello.name", 0, 0)
             if ConnectedTello.name == "Tello_B":
-                cv2.imshow(ConnectedTello.name, imgFT)
-                cv2.moveWindow(ConnectedTello.name, 0, 300)
+                cv2.imshow("ConnectedTello.name", imgFT)
+                cv2.moveWindow("ConnectedTello.name", 0, 300)
             if ConnectedTello.name == "Tello_C":
-                cv2.imshow(ConnectedTello.name, imgFT)
-                cv2.moveWindow(ConnectedTello.name, 0, 600)
+                cv2.imshow("ConnectedTello.name", imgFT)
+                cv2.moveWindow("ConnectedTello.name", 0, 600)
         else:
-
-            print(ConnectedTello.name + ': OBJECT OF INTEREST ADDED TO TASK REQUESTS')
+            print(ConnectedTello.name + ': ' + alert)
         cv2.waitKey(5)
 
 
