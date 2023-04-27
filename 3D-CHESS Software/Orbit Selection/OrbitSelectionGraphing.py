9a3e27b40ad1c9000d64f52f77d2b2b7b41f6f32
JasonSaladiner <saladiner.jason@tamu.edu>
2023-04-26 23:57:47 -0500

    graphing

--- 3D-CHESS Software/Orbit Selection/OrbitSelectionGraphing.py
+++ 3D-CHESS Software/Orbit Selection/OrbitSelectionGraphing.py
@@ -3,21 +3,23 @@ import numpy as np
 
 
 def plotWithAnnotate(results):
-    x = results[:,1]
-    y = results[:,2]
+    x = results[:,4]
+    y = results[:,5]
+    alt = results[:,0]
+    sats = results[:,1]
+    planes = results[:,2]
+    #f = results[:,3]
     names = []
-    c = []
     for i in range(len(results)):
-        #print(results[i])
-        names.append("(%i,%i,%i)"%(results[i][1],results[i][2],results[i][3]))
-        #c.append(results[i][0][0])
-
-    #norm = plt.Normalize(1,4)
+        names.append("%ikm:(%i,%i,%i)"%(alt[i],sats[i],planes[i],1))
+    
+    c = alt
+    norm = plt.Normalize(np.amin(c),np.amax(c))
     cmap = plt.cm.RdYlGn
 
     fig,ax = plt.subplots()
-    sc = plt.scatter(x,y,cmap=cmap)
-
+    sc = plt.scatter(x,y,c=c,cmap=cmap,norm=norm)
+    cb = plt.colorbar(orientation='vertical',label="Altitude")
     annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
                         bbox=dict(boxstyle="round", fc="w"),
                         arrowprops=dict(arrowstyle="->"))
@@ -51,9 +53,9 @@ def plotWithAnnotate(results):
     fig.canvas.mpl_connect("motion_notify_event", hover)
 
 
-    plt.title("Cost (USD) vs Revist Time (Hrs) for different ")
-    plt.xlabel("Revist Time (hr)")
-    plt.ylabel("Cost (USD)")
+    plt.suptitle("Cost (USD) vs Revisit Time (Hrs) for different constellations",size=20)
+    plt.xlabel("Revist Time (hr)",size=15)
+    plt.ylabel("Cost (USD)",size=15)
     plt.show()
 
 def readCSV():
@@ -70,6 +72,26 @@ def readCSV():
     return np.array(data)
 
 
+
+def SSOinclination(alt:float) -> float:
+    """
+    SSO inclination will return the required Sun-Synchronous Inclination (degrees) as a functino of altitude (in KM) using J2 pertubation
+    Inputs:
+        -Altitude in KM
+    Output:
+        -Inclination in degrees
+    """
+    from math import pi,sqrt,acos
+    Re = 6378.1     #Radius of Earth in KM
+    mu = 3.986e14   #Mu of earth in m^3/s^2
+
+    J2 = 1.08262668e-3
+    omegaDot = 2*pi/365/86400
+
+    a = (alt+Re)*1000
+    n = sqrt(mu/pow(a,3))
+    return acos(-2/3 * omegaDot/n/J2 * pow(a/Re/1000,2))*180/pi
+print(SSOinclination(500))
 results = (readCSV())
-print(len(results))
-plotWithAnnotate(results)
+#print(len(results))
+#plotWithAnnotate(results)
